24cddd7c60d105d28e80bcfa3aad8bffe8a3c87a
fabriciovo_dws <fabricio.varisco@dentsu.com>
2023-06-09 15:11:27 -0300

    Initial Commit

---
+++ cpp-synthesizing-natural-textures/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpp-synthesizing-natural-textures
+//
+//  Created by fabricio varisco oliveira on 09/06/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
